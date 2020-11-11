5b2c71e04a7fd275f9207a7832023e53b0a84a25
Soumikpal9 <soumikpal1997@gmail.com>
2020-11-12 02:07:29 +0530

    [SP]Added: MongoDB Implemented To COllect The Data And Connected To Server

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,24 @@
 var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+
+//Connect To The Database
+mongoose.connect("mongodb+srv://test:test@todo.x6opt.mongodb.net/todo?retryWrites=true&w=majority&tls=true", {useUnifiedTopology: true,
+                                                                                                              useNewUrlParser: true,}).then(() => console.log('DB Connected!'))
+                                                                                                              .catch(err => {
+                                                                                                                   console.log('DB Connection Error: ' + err)
+                                                                                                              })
+
+//Create A Schema- This is like a blueprint 
+var todoSchema = new mongoose.Schema({
+    item: String
+})
+
+//create A Model
+var Todo = mongoose.model('Todo', todoSchema)
+var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+    if(err) throw err
+    console.log('item saved')
+})
 
 var data = [{item: 'get milk'},
             {item: 'walk dog'},
